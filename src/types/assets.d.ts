bfe65962e3a8094f822194b7f27cd181a38bd31f
Miguel Pombo <mp96dev@gmail.com>
2026-02-10 16:11:40 -0300

    feat: add assets types definition

---
+++ src/types/assets.d.ts
@@ -0,0 +1,11 @@
+declare module "*.css";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.svg" {
+  import * as React from "react";
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
